86f6d8fa8c2f8dd4086712a432ecf7be9094a4ef
hujinguang <hujinguang@jd.com>
2016-08-10 14:20:43 +0800

    react activity

--- app/src/main/java/com/stu/heweather/MainActivity.java
+++ app/src/main/java/com/stu/heweather/MainActivity.java
@@ -1,31 +1,16 @@
 package com.stu.heweather;
 
 import android.content.Intent;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
 import android.widget.Button;
 
-import com.github.simonpercic.oklog3.OkLogInterceptor;
 import com.jakewharton.rxbinding.view.RxView;
-import com.stu.heweather.model.Const;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.FormBody;
-import okhttp3.HttpUrl;
-import okhttp3.Interceptor;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     @InjectView(R.id.btn)
     Button btn;
+    @InjectView(R.id.btnReact)
+    Button btnReact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +33,16 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void call(Void aVoid) {
                         Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+        RxView.clicks(btnReact)
+                .throttleWithTimeout(1000, TimeUnit.MILLISECONDS)
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        Intent intent = new Intent(MainActivity.this, MyReactActivity.class);
                         startActivity(intent);
                     }
                 });
